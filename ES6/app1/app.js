00c7c0d39886e83e58be74affcce1dbbe8b59897
ALESSANDRO DE OLIVEIRA LEMOS <45742267+aolCarioca@users.noreply.github.com>
2020-05-14 18:25:20 -0300

    última atualização  14/05/2020

--- ES6/app1/app.js
+++ ES6/app1/app.js
@@ -60,12 +60,59 @@ class Bd {
             //neste caso iremos pular esses indices 
             if(despesa === null){
                 continue
-            }  
-
+            }
+            
+            despesa.id = i 
             despesas.push(despesa)
         }
 
         return despesas
+    }
+    pesquisar(despesa){
+
+        let despesasFiltradas = Array()
+
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+        console.log(despesa)
+
+        console.log(despesasFiltradas)
+     
+        //ano
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        
+        //mes
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes== despesa.mes)          
+        }
+        
+        //dia
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)          
+        }
+        
+        //tipo
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)           
+        }
+        
+        //descricao
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)           
+        }
+
+        //valor
+        if(despesa.valor != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)           
+        }
+
+        return despesasFiltradas    
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
@@ -100,6 +147,13 @@ function cadastrarDesepesa(){
         document.getElementById('modal_btn').className = 'btn btn-success'        
         //dialog de sucesso
         $('#modalRegistraDespesa').modal('show')
+
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
     } else {
       
         document.getElementById('modal_titulo').innerHTML = 'Erro na gravação'
@@ -113,14 +167,15 @@ function cadastrarDesepesa(){
     }   
 }
 
-function carregaListaDespesas(){
+function carregaListaDespesas(despesas = Array(), filtro = false){
 
-    let despesas = Array()
-    
-    despesas = bd.recuperarTodosRegistros()
+    if(despesas.length == 0 && filtro == false) {
+        despesas = bd.recuperarTodosRegistros()
+    } 
 
     //seleciona o elemento tbody da tabela
     let listaDespesas = document.getElementById('listaDespesas')
+    listaDespesas.innerHTML = ''
 
     /*
     <tr>
@@ -158,6 +213,41 @@ function carregaListaDespesas(){
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criar botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+            //remocer as despesas
+            let id = this.id.replace('id_despesa_', '')
+
+            //alert(id)
+            
+            bd.remover(id)
+            
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
     })
+}
+
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value 
+    let mes = document.getElementById('mes').value 
+    let dia = document.getElementById('dia').value 
+    let tipo = document.getElementById('tipo').value 
+    let descricao = document.getElementById('descricao').value 
+    let valor = document.getElementById('valor').value 
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    let despesas = bd.pesquisar(despesa)
+
+    this.carregaListaDespesas(despesas, true)
+
+
+
 }
 
